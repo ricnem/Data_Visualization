708f80e2045391311b00313d330e3385f84e92b8
Nemuel <nemuel1337@gmail.com>
2022-06-08 23:52:52 +0800

    Visualization Introduction

--- Chapter 15 - Generating Data/rw_visual_multiple.py
+++ Chapter 15 - Generating Data/rw_visual_multiple.py
@@ -11,11 +11,17 @@ while True:
 
     # Plot the points in the walk
     plt.style.use('classic')
-
     fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, s=15)
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+
+    # Emphasize the first and last points.
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
     plt.show()
 
     keep_running = input("Make another walk? y/n: ")
     if keep_running == 'n':
+        print("Bitch ass mofucka")
         break
